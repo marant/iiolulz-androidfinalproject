c2a8a499d753f335934553a43f570856d19843f3
Marko Silokunnas <marko.silokunnas@gmail.com>
2010-12-12 14:47:12 +0200

    Changed Note.java to use Calendar instead of Date

--- src/fi/jamk/e6379/Note.java
+++ src/fi/jamk/e6379/Note.java
@@ -1,17 +1,17 @@
 package fi.jamk.e6379;
 
-import java.util.Date;
+import java.util.Calendar;
 
 public class Note {
-	private Date date;
+	private Calendar date;
 	private String noteText;
 	private String title;
 	
-	public Date getDate() {
+	public Calendar getDate() {
 		return date;
 	}
 	
-	public void setDate(Date date) {
+	public void setDate(Calendar date) {
 		this.date = date;
 	}
 	
